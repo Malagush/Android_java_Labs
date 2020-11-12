c963ec70cf5920969916469320db2ba4a39aa728
alexey <maksss071@gmail.com>
2020-11-12 09:44:10 +0700

    Закончил лабораторную №3 (загрузках всех файлов)

---
+++ Lab3_RecyclerView/app/src/main/java/com/example/lab3_recyclerview/DetailActivity.java
@@ -0,0 +1,22 @@
+package com.example.lab3_recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Bundle arguments = getIntent().getExtras();
+        String name = arguments.get("ARG_ITEM_ID").toString();
+
+        TextView textView = findViewById(R.id.detals);
+        textView.setText(name);
+
+    }
+}
